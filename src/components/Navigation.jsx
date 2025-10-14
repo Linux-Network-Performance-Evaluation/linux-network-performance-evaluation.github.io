11410cd75693d23d266c62831a68763972e65540
Thomas <TankedThomas@users.noreply.github.com>
2025-10-15 08:36:58 +1300

    Add active route tracking for tabs

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -1,27 +1,33 @@
-import { Container, Nav, Navbar } from "react-bootstrap"
-import { Link } from "react-router-dom"
+import { Container, Nav, Navbar } from "react-bootstrap";
+import { Link, useLocation } from "react-router-dom";
+import logo from '/assets/logo.png';
 
 function Navigation() {
+
+    const location = useLocation()
 
     return (
         <Navbar expand="sm" className="fixed-top justify-content-end">
             <Container>
-                <Navbar.Brand as={Link} to="/">Home</Navbar.Brand>
-                <Nav variant="tabs" defaultActiveKey="/">
+                <Navbar.Brand as={Link} to="/"><img src={logo} alt="Logo" /></Navbar.Brand>
+                <Nav variant="tabs">
                     <Nav.Item as="li" className="mx-2 mx-md-1">
-                        <Nav.Link as={Link} to="/research-upskilling">Research & Upskilling</Nav.Link>
+                        <Nav.Link as={Link} to="/" active={location.pathname === '/'}>About</Nav.Link>
                     </Nav.Item>
                     <Nav.Item as="li" className="mx-2 mx-md-1">
-                        <Nav.Link as={Link} to="/planning-control">Planning & Control</Nav.Link>
+                        <Nav.Link as={Link} to="/research-upskilling" active={location.pathname === '/research-upskilling'}>Research & Upskilling</Nav.Link>
                     </Nav.Item>
                     <Nav.Item as="li" className="mx-2 mx-md-1">
-                        <Nav.Link as={Link} to="/communication-teamwork">Communication & Teamwork</Nav.Link>
+                        <Nav.Link as={Link} to="/planning-control" active={location.pathname === '/planning-control'}>Planning & Control</Nav.Link>
                     </Nav.Item>
                     <Nav.Item as="li" className="mx-2 mx-md-1">
-                        <Nav.Link as={Link} to="/development-qa">Development & Quality Assurance</Nav.Link>
+                        <Nav.Link as={Link} to="/communication-teamwork" active={location.pathname === '/communication-teamwork'}>Communication & Teamwork</Nav.Link>
                     </Nav.Item>
                     <Nav.Item as="li" className="mx-2 mx-md-1">
-                        <Nav.Link as={Link} to="/poster">Poster</Nav.Link>
+                        <Nav.Link as={Link} to="/development-qa" active={location.pathname === '/development-qa'}>Development & Quality Assurance</Nav.Link>
+                    </Nav.Item>
+                    <Nav.Item as="li" className="mx-2 mx-md-1">
+                        <Nav.Link as={Link} to="/poster" active={location.pathname === '/poster'}>Poster</Nav.Link>
                     </Nav.Item>
                 </Nav>
             </Container>
